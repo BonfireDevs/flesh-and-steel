6d5d79b64c77f1cb0b1198c63f58e7b22641a16e
govindrajawat <govindrajawat260398@gmail.com>
2026-03-14 13:18:08 +0530

    version 2.9-fix

--- kubejs/server_scripts/loot_tables.js
+++ kubejs/server_scripts/loot_tables.js
@@ -8,16 +8,4 @@ LootJS.modifiers((event) => {
         .addLootTableModifier(/minecraft:chests\/(simple_dungeon|pillager_outpost|abandoned_mineshaft)/)
         .addLoot('immersive-guns:component_pistol')
         .randomChance(0.25);
-
-    // Add "Unidentified Radio Disc" to loot
-    // This item will be identify a random song from the audio_player folder when used
-    event
-        .addLootTableModifier(/minecraft:chests\/(simple_dungeon|pillager_outpost|village\/village_weaponsmith)/)
-        .addLoot(Item.of('minecraft:music_disc_fragment5', '{display:{Name:\'{"text":"Mysterious Radio Disc","color":"gold","italic":true}\',Lore:[\'{"text":"Right-click to identify...","color":"gray","italic":false}\']},UnidentifiedRadio:true}'))
-        .randomChance(0.15); // 15% chance in chests
-
-    event
-        .addLootTypeModifier(LootType.ENTITY)
-        .addLoot(Item.of('minecraft:music_disc_fragment5', '{display:{Name:\'{"text":"Mysterious Radio Disc","color":"gold","italic":true}\',Lore:[\'{"text":"Right-click to identify...","color":"gray","italic":false}\']},UnidentifiedRadio:true}'))
-        .randomChance(0.01); // 1% chance from monsters
 });
